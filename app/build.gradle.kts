3fbccda4b6f626167f5417d7a1f5c8ed6d739b35
sls1005 <sls1005@github.com>
2024-02-09 14:13:00 +0800

    Increase version number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "test.com.github.www.sls1005.filesaver"
         minSdk = 21
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
